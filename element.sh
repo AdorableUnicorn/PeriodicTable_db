e614c7410a14db104b6b690edc5c841ef59a9e36
Anna Kutsa <60103129+AdorableUnicorn@users.noreply.github.com>
2025-03-30 20:55:05 +0200

    Update element.sh
    finished the script

--- element.sh
+++ element.sh
@@ -1,33 +1,40 @@
 #!/bin/bash
 
-echo "Please provide an element as an argument."
-
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
-#atomic number #symbol #name
-
-if [[ ! -z $1 ]]
+if [[ -z $1 ]]
 then
 
+echo "Please provide an element as an argument."
+
+else
+
+COMPLETED_LOOP=true
 ARGUMENT="$($PSQL "select * from elements;")"
 
 
-echo "$ARGUMENT" | while IFS="|"  read -r ATOMIC_NUMBER SYMBOL NAME
+while IFS="|"  read -r ATOMIC_NUMBER SYMBOL NAME
 do
 
-if [[ $ATOMIC_NUMBER==$1 ]] || [[ $SYMBOL==$1 ]] || [[ $NAME==$1 ]]
+if [[ $ATOMIC_NUMBER = $1 ]] || [[ $SYMBOL = $1 ]] || [[ $NAME = $1 ]]
 then
 
-PROPERTIES="$($PSQL "select atomic_mass, melting_point_celsius, boiling_point_celsius from properties where atomic_number=$ATOMIC_NUMBER;")"
-IFC="|" read -r MASS MPC BPC <<< $PROPERTIES
+PROPERTIES="$($PSQL "SELECT atomic_mass, melting_point_celsius, boiling_point_celsius, type FROM properties LEFT JOIN types ON properties.type_id = types.type_id WHERE atomic_number=$ATOMIC_NUMBER;")"
 
-echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a nonmetal, with a mass of $MASS amu. $NAME has a melting point of $MPC celsius and a boiling point of $BPC celsius."
+IFS="|" read MASS MPC BPC TYPE <<< $PROPERTIES
+
+echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MPC celsius and a boiling point of $BPC celsius."
+COMPLETED_LOOP=false
+
 break
+
 fi
 
+done < <(echo "$ARGUMENT") # Notes for future me: used process substitution istead od direct pipe, as pipe creates subshell scope 
+
+if $COMPLETED_LOOP
+then 
 echo "I could not find that element in the database."
-
-
-done
+fi
 
 fi
